c1edfb2ed5fe919449ee2671a840872ba5901a5a
yirmiri <yirmirilucy@gmail.com>
2024-06-25 01:30:06 -0400

    Initial Commit

---
+++ src/main/java/net/yirmiri/urban_decor/UrbanDecorDataGenerator.java
@@ -0,0 +1,11 @@
+package net.yirmiri.urban_decor;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UrbanDecorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
