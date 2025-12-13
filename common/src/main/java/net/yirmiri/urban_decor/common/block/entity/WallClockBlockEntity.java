502eb0718a5d703aed3c76eda681c65e14b475cf
Yirmiri <yirmirilucy@gmail.com>
2025-12-13 11:48:15 -0500

    todo

--- common/src/main/java/net/yirmiri/urban_decor/common/block/entity/WallClockBlockEntity.java
+++ common/src/main/java/net/yirmiri/urban_decor/common/block/entity/WallClockBlockEntity.java
@@ -15,10 +15,10 @@ public class WallClockBlockEntity extends BlockEntity {
     }
 
     public static void tick(Level level, BlockPos pos, BlockState state, WallClockBlockEntity blockEntity) {
-        if (!level.isClientSide) {
-            if (level.getDayTime() % 1000L == 0 && level.getGameRules().getBoolean(GameRules.RULE_DAYLIGHT)) {
-                level.playSound(null, pos, SoundEvents.NOTE_BLOCK_BELL.value(), SoundSource.BLOCKS, 0.5F, -1.0F);
-            }
+        if (!level.isClientSide) { //grand clock instead will play tong every 6 hours at low volume short range and differ in pitch per hour
+//            if (level.getDayTime() % 1000L == 0 && level.getGameRules().getBoolean(GameRules.RULE_DAYLIGHT)) {
+//                level.playSound(null, pos, SoundEvents.NOTE_BLOCK_BELL.value(), SoundSource.BLOCKS, 0.5F, -1.0F);
+//            }
 //            else if (level.getDayTime() % 20L == 0) {
 //                level.playSound(null, pos, SoundEvents.UI_BUTTON_CLICK.value(), SoundSource.BLOCKS, 0.2F, 1.0F);
 //            }
